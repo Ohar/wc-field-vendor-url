29a380def497b787fbce58032ae5aaf89a6593f3
Pavel Lysenko <code@ohar.name>
2016-12-12 00:25:25 +0300

    Add clickable link

--- wc-field-vendor-url.php
+++ wc-field-vendor-url.php
@@ -6,7 +6,7 @@ Description:  Add custom field “vendor_url” to the WooCommerce products
 Author: Pavel Lysenko aka Ohar
 Author URI: http://ohar.name/
 Contributors: ohar
-Version: 1.0.6
+Version: 1.0.7
 License: MIT
 Text Domain: wc-field-vendor-url
 Domain Path: /languages
@@ -23,13 +23,17 @@ add_action( 'woocommerce_process_product_meta', 'save_custom_woocommerce_general
 function add_custom_woocommerce_general_field_vendor_url() {
 
   global $woocommerce, $post;
+	
+	$post_id = $post->ID;
+	$vendor_url = get_post_meta($post_id, 'vendor_url', true);
+	$link = $vendor_url ? " <a target='_blank' href='$vendor_url'>ссылка</a>" : '';
 
   echo '<div class="options_group">';
 
 	woocommerce_wp_text_input(
 		array(
 			'id'                => 'vendor_url',
-			'label'             => __( 'Vendor URL', 'wc-field-vendor-url' ),
+			'label'             => __( 'Vendor URL', 'wc-field-vendor-url' ).$link,
 			'placeholder'       => 'http://example.com/item',
 			'desc_tip'          => 'true',
 			'description'       => __( 'URL of product on the vendor`s site', 'wc-field-vendor-url' ),
